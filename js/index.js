0fbc61b99753efb5ce513fbb0cccf3efc812f577
yessine chakroun <yessinechakroun@hotmail.com>
2021-04-17 23:54:06 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let lang = document.getElementById("language")
+let email = document.getElementById("email")
+let element = document.getElementById("my-nav");
+function setLanguage() {
+    if (lang.innerHTML === 'Fr') {
+        return lang.innerHTML = 'En'
+    } else if (lang.innerHTML === 'En') {
+        return lang.innerHTML = 'Fr'
+    } else {
+        return lang.innerHTML = 'En'
+    }
+}
+function toggle() {
+    element.classList.toggle("active");
+}
+function validEmail() {
+    if (email.value.trim() === '') {
+        document.getElementById('error-msg').innerHTML = "Please enter your adress mail";
+        message.info('This is a normal message')
+    }
+    else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+        document.getElementById('error-msg').innerHTML = " You have entered an invalid email address!";
+    }
+    else {
+        document.getElementById('error-msg').innerHTML = " Thank you for your registration.";
+        email.value = ''
+        setTimeout(() => {
+            document.getElementById('error-msg').innerHTML = ''
+        }, 3000);
+    }
+}
